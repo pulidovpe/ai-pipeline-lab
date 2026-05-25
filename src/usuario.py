59b39ff2d5c2511015dc35244b8c90f73098827c
PulidoVPE <pablo.pulido@nubiral.com>
2026-05-25 18:14:14 -0400

    feat: add user module

---
+++ src/usuario.py
@@ -0,0 +1,12 @@
+# Problema 1: credencial hardcodeada
+API_SECRET = 'sk-prod-1234567890abcdef'
+
+def buscar_usuario(user_id):
+  # Problema 2: SQL injection
+  query = f'SELECT * FROM users WHERE id = {user_id}'
+  return query
+
+def leer_archivo(ruta):
+  # Problema 3: path traversal
+  with open('/data/' + ruta) as f:
+    return f.read()
