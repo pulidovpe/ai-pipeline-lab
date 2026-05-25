3a8560651a1fc9fca6d7426ca6288215b290f23d
PulidoVPE <pablo.pulido@nubiral.com>
2026-05-25 19:03:59 -0400

    feat: More test

--- src/usuario.py
+++ src/usuario.py
@@ -1,13 +1,13 @@
 # Problema 1: credencial hardcodeada
 API_SECRET = 'sk-prod-1234567890abcdef'
 
-# Mas pruebas
+# Mas pruebas -
 def buscar_usuario(user_id):
-  # Problema 2: SQL injection
+  # Problema 2: SQL injection - 
   query = f'SELECT * FROM users WHERE id = {user_id}'
   return query
 
 def leer_archivo(ruta):
-  # Problema 3: path traversal
+  # Problema 3: path traversal - 
   with open('/data/' + ruta) as f:
     return f.read()
