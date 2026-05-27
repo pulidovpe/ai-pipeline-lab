fa645ffe2c43762b1c89219705faab84c941747e
PulidoVPE <pablo.pulido@nubiral.com>
2026-05-27 19:53:58 -0400

    bug: calcular_promedio sin validacion de lista vacia

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,4 +1,7 @@
 def dividir(a, b):
-  return a / b # Sin ZeroDivisionError
+    if b == 0:
+        raise ZeroDivisionError("division by zero")
+    return a / b
+
 def calcular_promedio(numeros):
-  return sum(numeros) / len(numeros) # Sin ValueError para lista vacia
+    return sum(numeros) / len(numeros)
