100f45123aef9b0836edca07e3240953a6047327
PulidoVPE <pablo.pulido@nubiral.com>
2026-05-27 19:48:59 -0400

    Bug: Nuevo cambio

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,4 +1,6 @@
+## Nuevo cambio
 def dividir(a, b):
   return a / b # Sin ZeroDivisionError
+
 def calcular_promedio(numeros):
   return sum(numeros) / len(numeros) # Sin ValueError para lista vacia
