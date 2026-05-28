8553b8239f64f8c4bcff7884131729d7623fba25
PulidoVPE <pablo.pulido@nubiral.com>
2026-05-27 20:26:19 -0400

    bug: sin validacion lista vacia

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,7 +1,9 @@
 def dividir(a, b):
+    # nuevos cambios
     if b == 0:
         raise ZeroDivisionError("division by zero")
     return a / b
 
 def calcular_promedio(numeros):
+    # mas cambios
     return sum(numeros) / len(numeros)
